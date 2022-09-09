fb5da544cd39029e8f9a34c1d5b609f1cc2b46e3
Mark Mwenesi <markmwenesi001@gmail.com>
2022-09-09 02:14:28 -0700

    Using if else to check if a number is positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,20 +4,13 @@
 
 #include <stdio.h>
 
-
-
 /**
-
 * main - print if the number is postive, zero, or negative
-
 *
-
 * Description: using the main function
-
+* 
 * this program prints "Programming is positive, zero, or negative
-
 * Return: 0
-
 */
 
 int main(void)
