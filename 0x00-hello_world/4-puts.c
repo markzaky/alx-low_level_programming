0c34f9b00ac8cb707cd9a048bce1ee4346e29fb6
Mark Mwenesi <markmwenesi001@gmail.com>
2022-09-09 00:46:32 -0700

    Print without using puts correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,18 +3,14 @@
 
 
 /**
+ *  main - print the string in the put function
  *
- * * main - print the string in the put function
+ *  Description: using the main function
  *
- * *
+ *  this program prints "Programming is like building a multilingual puzzle
  *
- * * Description: using the main function
- *
- * * this program prints "Programming is like building a multilingual puzzle
- *
- * * Return: 0
- *
- * */
+ *  Return: 0
+ */
 
 int main(void)
 
