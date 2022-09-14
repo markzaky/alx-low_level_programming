974d1927bc1e7355aa688678dec57d7219efbfd2
Mark Mwenesi <markmwenesi001@gmail.com>
2022-09-14 01:36:04 -0700

    a function that prints a to z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main-print the alphabet
+ * Return:0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
+}
