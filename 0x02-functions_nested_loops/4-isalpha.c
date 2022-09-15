058a1589e3e5778edd2db4a070be7ef7bde6a922
Mark Mwenesi <markmwenesi001@gmail.com>
2022-09-15 02:17:28 -0700

    Check ig c is an alphabet letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - function that checks if c is an alphabet letter
+ * @c: used as argument fir function
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z' || (c >= 'A' && c <= 'Z'))
+			{
+			return (1);
+			}
+			else
+			{
+			return (0);
+			}
+}
