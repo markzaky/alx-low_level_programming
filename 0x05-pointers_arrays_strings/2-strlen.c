919885bde6d78662703d34a176cf8f4b127defd3
Mark Mwenesi <markmwenesi001@gmail.com>
2022-09-19 23:55:01 -0700

    Function that returns the string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen -a function that returns the lengt of a string
+ * @c: pointer to the string varriable
+ *
+ * Return: the legth of string c
+ */
+
+int _strlen(char *s);
+{
+int i = 0;
+while (s[i])
+	i++;
+return (i);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,5 +3,6 @@
 
 int _putchar(char c);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 
 #endif
